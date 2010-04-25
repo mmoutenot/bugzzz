e8f4902d4ff0e7464e0b7d13b32a4a14b6ed0cd8
mmoutenot <mmoutenot@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-04-25 00:33:47 +0000

    new sprite for dual layer testing

---
+++ bugzzz/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Bugzzz
+{
+    class Player
+    {
+        public float p_rotation = 0.0f;
+        public float p_rotation_b = 0.0f;
+        public Texture2D p_spriteT = null;
+        public Texture2D p_spriteB = null;
+        public Vector2 p_velocity = Vector2.Zero;
+        public Vector2 p_position = Vector2.Zero;
+        public bool p_fire;
+        private int p_id;
+
+        public Player(int id, Texture2D spriteT, Texture2D spriteB)
+        {
+            p_id = id; 
+            p_fire = false;
+            p_position.X = 200;
+            p_position.Y = 200;
+            p_velocity = Vector2.Zero;
+            p_spriteT = spriteT;
+            p_spriteB = spriteB;
+            p_rotation = 0.0f;
+            p_rotation_b = 0.0f;
+        }
+    }
+}
