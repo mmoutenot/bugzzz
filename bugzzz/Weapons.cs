0383829e9028f98bc0dfee468003693dd32ac9dc
mmoutenot <mmoutenot@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-04-28 01:23:15 +0000

    Added weapon pickup framework. Shotgun inplemented, but delay needs to be increased to prevent lag. Flamethrower still needs a lot of work.

---
+++ bugzzz/Weapons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Bugzzz
+{
+    class Weapons
+    {
+        public string name;
+        public int damage;
+        public int id;
+        Texture2D[] sprite;
+
+        public Weapons(Texture2D a, Texture2D b, Texture2D c)
+        {
+            sprite = new Texture2D[3];
+            sprite[0] = a;
+            sprite[1] = b;
+            sprite[2] = c;
+            id = 1;
+        }
+    }
+}
