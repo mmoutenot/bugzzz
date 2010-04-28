a92f9dab35b66b247eab7eb7c93c76171102c697
mmoutenot <mmoutenot@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-04-28 02:24:36 +0000

    Started adding pickup ability. Collision not being detected? Probably a dumb mistake- taking a break

---
+++ trunk/bugzzz/WeaponPickup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Bugzzz
+{
+    class WeaponPickup
+    {
+        public Texture2D sprite;
+        public Vector2 position;
+        public bool alive;
+        public int weaponIndex;
+
+        public WeaponPickup(Texture2D loadedTexture)
+        {
+            sprite = loadedTexture;
+            alive = false;
+            position = Vector2.Zero;
+            weaponIndex = 0;
+        }
+    }
+}
