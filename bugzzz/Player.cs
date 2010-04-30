650a9ef20f1076fef3db9303d51a0d8a3b6814d2
mmoutenot <mmoutenot@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-04-30 19:01:43 +0000

    Got rid of the stupid naming convension I started with the p_ for all player variables. Also put initializing logic in the initialize function rather than in the loadContent function.
    Game is starting to lag... we need to make it smarter!

--- bugzzz/Player.cs
+++ bugzzz/Player.cs
@@ -16,37 +16,51 @@ namespace Bugzzz
 {
     class Player
     {
-        public float p_rotation = 0.0f;
-        public float p_rotation_b = 0.0f;
-        public Texture2D p_spriteT = null;
-        public Texture2D p_spriteB = null;
-        public Vector2 p_velocity = Vector2.Zero;
-        public Vector2 p_position = Vector2.Zero;
-        public bool p_fire;
-        private int p_id;
+        public float rotation = 0.0f;
+        public float rotation_b = 0.0f;
+        public Texture2D spriteT = null;
+        public Texture2D spriteB = null;
+        public Vector2 velocity = Vector2.Zero;
+        public Vector2 position = Vector2.Zero;
+
+        public bool fire;
+        public bool move1;
+        public bool move2;
+        public bool move3;
+
+        private int id;
         public bool deploy;
+        
         public int health;
+        public int energy;
+
         public int score;
         public Weapons weapon;
         public int activeWeapon;
-        public Statistics p_stat;
+        public Statistics stat;
         public int livesLeft;
 
-        public Player(int id, Texture2D spriteT, Texture2D spriteB, Weapons w, Vector2 pos, Statistics stat)
+        public Player(int i, Texture2D t, Texture2D b, Weapons w, Vector2 pos, Statistics s)
         {
+            energy = 100;
             deploy = false;
-            p_id = id; 
-            p_fire = false;
-            p_position = pos;
-            p_velocity = Vector2.Zero;
-            p_spriteT = spriteT;
-            p_spriteB = spriteB;
-            p_rotation = 0.0f;
-            p_rotation_b = 0.0f;
+            id = i;
+ 
+            fire = false;
+            move1 = false;
+            move2 = false;
+            move3 = false;
+
+            position = pos;
+            velocity = Vector2.Zero;
+            spriteT = t;
+            spriteB = b;
+            rotation = 0.0f;
+            rotation_b = 0.0f;
             health = 100;
             weapon = w;
             activeWeapon = 0;
-            p_stat = stat;
+            stat = s;
             livesLeft = 5;
         }
     }
