281c95dccf6f1ff7eb38d0dc60ffe977551c09c9
cmh.sparta <cmh.sparta@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-05-11 02:51:57 +0000

    Added HEalthBar Class, reduced chance for narcolepsy effect.

---
+++ bugzzz/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bugzzz
+{
+    class HealthBar
+    {
+
+
+
+
+        #region Main Methods
+
+        #endregion
+    }
+}
