db736d2e6e040f2bb2a226782bb34fde56883c3e
esetnik <esetnik@8f7b6635-4d5a-bc3e-cabb-7f792b579fc0>
2010-05-11 10:03:14 +0000

    Awesome level score code

--- bugzzz/LevelScore.cs
+++ bugzzz/LevelScore.cs
@@ -30,6 +30,11 @@ namespace Bugzzz
         bool spreeIsDone;
         bool livesLeftIsDone;
         bool gameOver;
+        int alphaAvgLife;
+        int alphaMaxSpree;
+        int alphaLivesLeft;
+        int alphaPressA;
+        bool ascendingAlpha;
 
         public LevelScore(int level, Player player1, Player player2, bool alive, int scrolltime, SpriteFont levelfont, SpriteFont titlefont, GraphicsDevice gDevice, Texture2D healthBar, bool gameOver)
         {
@@ -43,40 +48,45 @@ namespace Bugzzz
             this.s = new SpriteBatch(gDevice);
             this.healthBar = healthBar;
             this.gameOver = gameOver;
+            this.alphaAvgLife = 30;
+            this.alphaMaxSpree = 30;
+            this.alphaLivesLeft = 30;
+            this.alphaPressA = 30;
+            this.ascendingAlpha = true;
         }
 
         public void Draw(Viewport viewport)
         {
-            s.Begin();
+            s.Begin(SpriteBlendMode.AlphaBlend);
             int s1, s2;
             if (tempscore <= player1.score)
                 s1 = tempscore;
-            else 
+            else
                 s1 = player1.score;
             if (tempscore <= player2.score)
                 s2 = tempscore;
-            else 
+            else
                 s2 = player2.score;
 
-            string levelStr = "Level "+level;
+            string levelStr = "Level " + level;
             if (gameOver)
             {
                 levelStr = "Game Over";
             }
-            Vector2 textSize = levelfont.MeasureString(levelStr); 
-            Vector2 textCenter = new Vector2(viewport.Width / 2, 50); 
- 
-            s.DrawString(titlefont,levelStr,textCenter-(textSize/2), new Color(Color.Yellow,(byte)(200)));
-            s.Draw(healthBar, new Rectangle(viewport.Width / 8, viewport.Height / 4, viewport.Width*3 / 4, viewport.Height / 2), new Color(Color.DarkBlue, (byte)(50)));
-            
+            Vector2 textSize = levelfont.MeasureString(levelStr);
+            Vector2 textCenter = new Vector2(viewport.Width / 2, 50);
+
+            s.DrawString(titlefont, levelStr, textCenter - (textSize / 2), new Color(Color.Yellow, (byte)(200)));
+            s.Draw(healthBar, new Rectangle(viewport.Width / 8, viewport.Height / 4, viewport.Width * 3 / 4, viewport.Height / 2), new Color(Color.DarkBlue, (byte)(50)));
+
             // At the end of the level, halt the stats for p1,p2
             player1.stat.Started = false;
             player2.stat.Started = false;
-            
+
             string avgLifeP1 = string.Format("{0:0.0}", player1.stat.averageLifeTime());
             string avgLifeP2 = string.Format("{0:0.0}", player2.stat.averageLifeTime());
-            s.DrawString(levelfont, "Player 1", new Vector2((viewport.Width/8)+350,(viewport.Height/4)+40),new Color(Color.Yellow,(byte)(200)));
-            s.DrawString(levelfont, "Player 2", new Vector2(((7*viewport.Width)/8)-(levelfont.MeasureString("Player 2").X)-100, (viewport.Height / 4) + 40), new Color(Color.Yellow, (byte)(200)));
+            s.DrawString(levelfont, "Player 1", new Vector2((viewport.Width / 8) + 350, (viewport.Height / 4) + 40), new Color(Color.Yellow, (byte)(200)));
+            s.DrawString(levelfont, "Player 2", new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 40), new Color(Color.Yellow, (byte)(200)));
             /*
             s.DrawString(levelfont, "Score:       " + s1 + "                   " + s2, new Vector2((viewport.Width / 4) + 10, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
             s.DrawString(levelfont, "Avg Life:    " + avgLifeP1 + "s                  " + avgLifeP2+"s", new Vector2((viewport.Width / 4) + 10, (viewport.Height / 4) + 150), new Color(Color.Red, (byte)(200)));
@@ -85,14 +95,76 @@ namespace Bugzzz
             
              */
             s.DrawString(levelfont, "Score:\nAvg Life:\nMax Spree:\nLives Left:", new Vector2((viewport.Width / 8) + 75, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
-            s.DrawString(levelfont, s1+"", new Vector2((viewport.Width / 8) + 350, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
-            s.DrawString(levelfont, s2+"", new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
+            s.DrawString(levelfont, s1 + "", new Vector2((viewport.Width / 8) + 350, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
+            s.DrawString(levelfont, s2 + "", new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 100), new Color(Color.Red, (byte)(200)));
             if (scoreIsDone)
             {
-                s.DrawString(levelfont, avgLifeP1 + "\n" + player1.stat.MaxSpreeLength + "\n" + player1.healthBar.LivesLeft, new Vector2((viewport.Width / 8) + 350, (viewport.Height / 4) + 150), new Color(Color.Red, (byte)(200)));
-                s.DrawString(levelfont, avgLifeP2 + "\n" + player2.stat.MaxSpreeLength + "\n" + player2.healthBar.LivesLeft, new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 150), new Color(Color.Red, (byte)(200)));
+                s.DrawString(levelfont, avgLifeP1, new Vector2((viewport.Width / 8) + 350, (viewport.Height / 4) + 150), new Color(Color.Red, (byte)(alphaAvgLife)));
+                s.DrawString(levelfont, avgLifeP2, new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 150), new Color(Color.Red, (byte)(alphaAvgLife)));
+                if (alphaAvgLife > 100)
+                {
+                    avgLifeIsDone = true;
+                }
+                if (alphaAvgLife < 200)
+                {
+                    alphaAvgLife++;
+                }
             }
-             if (tempscore < player1.score || tempscore < player2.score)
+            if (avgLifeIsDone)
+            {
+                s.DrawString(levelfont,player1.stat.MaxSpreeLength+"", new Vector2((viewport.Width / 8) + 355, (viewport.Height / 4) + 200), new Color(Color.Red, (byte)(alphaMaxSpree)));
+                s.DrawString(levelfont,player2.stat.MaxSpreeLength+"", new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 200), new Color(Color.Red, (byte)(alphaMaxSpree)));
+                if (alphaMaxSpree > 100)
+                {
+                    spreeIsDone = true;
+                }
+                if (alphaMaxSpree < 200)
+                {
+                    alphaMaxSpree++;
+                }
+            }
+            if (spreeIsDone)
+            {
+                s.DrawString(levelfont, player1.healthBar.LivesLeft+"", new Vector2((viewport.Width / 8) + 360, (viewport.Height / 4) + 250), new Color(Color.Red, (byte)(alphaLivesLeft)));
+                s.DrawString(levelfont, player2.healthBar.LivesLeft+"", new Vector2(((7 * viewport.Width) / 8) - (levelfont.MeasureString("Player 2").X) - 100, (viewport.Height / 4) + 250), new Color(Color.Red, (byte)(alphaLivesLeft)));
+                if (alphaLivesLeft > 100)
+                {
+                    livesLeftIsDone = true;
+                }
+                if (alphaLivesLeft < 200)
+                {
+                    alphaLivesLeft++;
+                }
+            }
+            if (livesLeftIsDone)
+            {
+                s.DrawString(levelfont, "Press (A) to continue", new Vector2((viewport.Width / 4)+170, (3* viewport.Height / 4) - 50), new Color(Color.Blue, (byte)(alphaPressA)));
+                //Console.WriteLine("Alpha:" + alphaPressA);
+                if (ascendingAlpha)
+                {
+                    if (alphaPressA > 150)
+                    {
+                        ascendingAlpha = false;
+                    }
+                    else
+                    {
+                        alphaPressA+=4;
+                    }
+                }
+                else
+                {
+                    if (alphaPressA <= 30)
+                    {
+                        ascendingAlpha = true;
+                    }
+                    else
+                    {
+                        alphaPressA-=4;
+                    }
+                }
+
+            }
+            if (tempscore < player1.score || tempscore < player2.score)
             {
                 if (tempscore < 200)
                     tempscore += 5;
@@ -109,4 +181,5 @@ namespace Bugzzz
             s.End();
         }
     }
+
 }
